57b85abfee3a5c491b026156a36e6e5dd91ad021
Matin <MatiinBaloochestani@Gmail.com>
2021-10-15 20:51:07 +0330

    Change some texts

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher, executor, filters, types
 from aiogram.utils.emoji import emojize
-
+from pdb import set_trace
 import config
 from downloader import download
 
@@ -20,44 +20,38 @@ def convert_bytes(size):
 
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
-    await message.reply('Hi\n\nPlease send me name of your app')
+    await message.reply(
+        emojize(
+            'Welcome to @bazaar_dlbot :raised_hand:\n\n'\
+            ':check_mark_button: Please send me the app link in CafeBazaar.ir.\n\n\n'
+            ':information: Source code is available on '\
+            '[GitHub](https://github.com/Matin-B/bazaar_dlbot)'
+        )
+    )
 
 
 @dp.message_handler(filters.Regexp(regexp=r'cafebazaar\.ir\/app\/(.*)'))
 async def download_app(message: types.Message, regexp):
+    chat_id = message.chat.id
     package_name = regexp.group(1)
     download_response = download(package_name)
     if download_response['status'] == 'success':
         download_link = download_response['data']['download_link']
         package_size = download_response['data']['package_size']
         installation_size = download_response['data']['installation_size']
-        if int(package_size) >= 52428800:
-            await message.reply(
-                emojize(
-                    ':information: Application filesize is above 50MB '\
-                    'so you can download only using link\n\n'\
-                    f':diamond_with_a_dot: Package Name: {package_name}\n'\
-                    f':small_orange_diamond: Package Size: {convert_bytes(int(package_size))}\n'\
-                    f':small_blue_diamond: Installation Size: {convert_bytes(int(installation_size))}\n\n'\
-                    f':link: Download Link:\n{download_link}\n\n'\
-                    ':robot: @bazaar_dlbot'
-                )
+        await message.reply(
+            emojize(
+                f':diamond_with_a_dot: Package Name: {package_name}\n'\
+                f':small_orange_diamond: Package Size: {convert_bytes(int(package_size))}\n'\
+                f':small_blue_diamond: Installation Size: {convert_bytes(int(installation_size))}\n\n'\
+                f':link: Download Link:\n{download_link}\n\n'\
+                ':robot: @bazaar_dlbot'
             )
-        else:
-            caption_text = f':diamond_with_a_dot: Package Name: {package_name}\n'\
-                    f':small_orange_diamond: Package Size: {convert_bytes(int(package_size))}\n'\
-                    f':small_blue_diamond: Installation Size: {convert_bytes(int(installation_size))}\n\n'\
-                    ':robot: @bazaar_dlbot'
-            await message.reply_document(
-                document=download_link,
-                caption=emojize(caption_text)
-            )
+        )
     else:
         await message.reply(
-            'مشکلی پیش آمده. لطفا دقایقی دیگر مجدد تلاش کنید.'
+            'Please try again in a few minutes.'
         )
-
-
 
 
 # Run bot
